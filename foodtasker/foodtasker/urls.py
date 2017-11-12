e5b4dc15a157d45fa5e878050cd7535f57255d7f
SandPipper <seerenf4@gmail.com>
2017-11-12 12:31:52 +0200

    Add auth system

--- foodtasker/foodtasker/urls.py
+++ foodtasker/foodtasker/urls.py
@@ -4,6 +4,9 @@ from django.contrib.auth import views as auth_views
 
 from foodtaskerapp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
@@ -14,7 +17,7 @@ urlpatterns = [
         {'next_page': '/'},
         name='restaurant-sign-out'),
     url(r'^restaurant/sign-up', views.restaurant_sign_up,
-        {'next_page': '/'},
+
         name='restaurant-sign-up'),
     url(r'^restaurant/$', views.restaurant_home, name='restaurant-home')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
