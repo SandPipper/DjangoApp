c086b3752551b298ed8672fb05a4946b148bf1db
SandPipper <seerenf4@gmail.com>
2017-11-18 18:10:54 +0200

    Add facebook authentication with create new user(customer or driver)

--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Customer, Driver
 
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
